967753620997a74cc97c00f53a235728a62aa725
Hung Arima <tronghung199@gmail.com>
2017-12-03 01:24:31 +0700

    Add 5.py

--- Session-4/Assignment/serious-ex/5.py
+++ Session-4/Assignment/serious-ex/5.py
@@ -1,8 +1,8 @@
-month1 = 1
+
 x1 = 0
-x2 = month1
+x2 = 1
 for i in range(5):
     x3 = x1 + x2
-    print("Month", i, ":", x3, "pair(s) of rabbit" )
     x1 = x2
     x2 = x3
+    print("Month", i, ":", x3, "pair(s) of rabbit" )
