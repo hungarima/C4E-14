76d4b0471893d8c011f026a347e0ffbcf3b88816
Hung Arima <tronghung199@gmail.com>
2017-12-07 18:47:23 +0700

    Modified 4.py

--- Session-5/lab1-assignment/4.py
+++ Session-5/lab1-assignment/4.py
@@ -11,15 +11,6 @@ customers = db['customers']
 lis_custs = customers.find()
 count = 0
 
-# Count the number of customers
-
-for customer in lis_custs:
-    if customer["ref"] == "ads" or customer["ref"] == "events" or customer["ref"] == "wom":
-        count+=1
-print("The number of customers group is:", count)
-
-#Pie_chart
-
 labels = ['Events', 'Advertisements', 'Word of mouth']
 colors = ['#FEBFB3', '#E1396C', '#96D38C']
 events = ads = wom = 0
@@ -29,8 +20,11 @@ for customer in lis_custs:
     elif customer["ref"] == "wom":
         wom += 1
     elif customer["ref"] == "events":
-        events +=1
+        events += 1
 data = [events, ads, wom]
+print("Events:", events)
+print("Ads:", ads)
+print("Wom:", wom)
 pyplot.pie(data, labels = labels, colors = colors, autopct='%1.1f%%', shadow = True)
 pyplot.axis('equal')
 pyplot.show()
